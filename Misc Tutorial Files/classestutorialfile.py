01c572143adecab72f526bab9f8a0b0ca1dd521a
Chunkyjoe <galbraithj66@gmail.com>
2020-03-31 13:44:41 -0400

    lol

--- Misc Tutorial Files/classestutorialfile.py
+++
@@ -1,6 +0,0 @@
-def optional_parameter(first_parameter = 1):
-    print(first_parameter + 5)
-
-optional_parameter(3)
-
-
--- Misc Tutorial Files/dictionarytutorialfile.py
+++
@@ -1,7 +0,0 @@
-mydictionary = {"name": "Joe", "height": "5'10\""}
-
-for key, value in mydictionary.items():
-    print(key)
-    print(value)
-
-
